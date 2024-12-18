57641dfc74c9d2c87f2f92069cbcff9de22dad51
glebvisage5 <efimkingleb@gmail.com>
2024-12-18 17:28:33 +0300

    update files

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const pool = require('./config/db');
@@ -12,7 +11,6 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
 
 // Маршруты
